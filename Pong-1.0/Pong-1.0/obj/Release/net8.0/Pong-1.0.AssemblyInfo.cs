c9ddac89ad41a849d14d8923729e1f2d01596fe8
AshrafBasnoe-Github <s1185527@student.windesheim.nl>
2024-05-19 17:03:08 +0200

    Switch Case
    Switch Case

--- Pong-1.0/Pong-1.0/obj/Release/net8.0/Pong-1.0.AssemblyInfo.cs
+++ Pong-1.0/Pong-1.0/obj/Release/net8.0/Pong-1.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pong-1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3fa07100e48cf248e1d10e7294950036c45d92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4387a874d78a054112a8e5678af761c890deaf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pong-1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pong-1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
