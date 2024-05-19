c97ddc28f7c4f6d02766898b54b326b79b2e0bf5
Bakkertje2003 <timbakkertje2@gmail.com>
2024-05-19 16:55:34 +0200

    begin code

---
+++ Pong-1.0/Pong-1.0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int fieldLength = 50, fieldWidth = 15;
+            const char fieldTile = '#';
+            string line = string.Concat(Enumerable.Repeat(fieldTile, fieldLength));
+
+            const int racketLength = fieldWidth / 4;
+            const char racketTile = '|';
+
+            int leftRacketHeight = 0;
+            int rightRacketHeight = 0;
+
+            
+        }
+    }
+}
