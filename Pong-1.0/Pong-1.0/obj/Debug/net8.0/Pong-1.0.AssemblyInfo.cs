3e59773d8b3c893261ed6219211d1ad675c59896
AshrafBasnoe-Github <s1185527@student.windesheim.nl>
2024-05-19 16:59:26 +0200

    assembly
    assembly

--- Pong-1.0/Pong-1.0/obj/Debug/net8.0/Pong-1.0.AssemblyInfo.cs
+++ Pong-1.0/Pong-1.0/obj/Debug/net8.0/Pong-1.0.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pong-1.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97ddc28f7c4f6d02766898b54b326b79b2e0bf5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3fa07100e48cf248e1d10e7294950036c45d92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pong-1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pong-1.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
